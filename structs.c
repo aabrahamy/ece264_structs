3ec97f10f64418d8e0fb9e56bafe1ef12586daf4
Ani Abrahamyan <aniabrahamyan800@gmail.com>
2026-06-26 14:31:09 -0400

    Add files via upload

---
+++ structs.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#define NUM_STUDENTS 10
+
+struct student {
+    int id;
+    char name[20];
+    float gpa;
+};
+
+int main() {
+    struct student students[NUM_STUDENTS] = {
+        {1,  "Alice", 3.5},
+        {2,  "Bob",   3.2},
+        {3,  "Carol", 3.9},
+        {4,  "David", 2.8},
+        {5,  "Eve",   3.7},
+        {6,  "Frank", 3.0},
+        {7,  "Grace", 3.6},
+        {8,  "Hank",  2.9},
+        {9,  "Ivy",   3.8},
+        {10, "Jack",  3.1}
+    };
+
+    struct student *ptr = students;
+
+    printf("ID   Name        GPA\n");
+    printf("----------------------\n");
+
+    //To do: print all student data to the table
+    //hint: use a for loop and a printf statement
+
+    return 0;
+}
